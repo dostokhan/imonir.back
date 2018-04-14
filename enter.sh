b356fb1bc16f36da17402558a5d9bde4c4ec9430
DostoKhan <dostokhan@gmail.com>
2018-04-14 22:22:40 +0600

    up down and deployment scripts and configs updated

--- enter.sh
+++ enter.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-docker-compose -f docker-compose.yml exec backend bash
+# docker-compose -f docker-compose.yml exec backend bash
+docker-compose exec backend bash
---
+++ logs.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# docker-compose -f docker-compose.yml logs --follow backend
+docker-compose logs --follow backend
